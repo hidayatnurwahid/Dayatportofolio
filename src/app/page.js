8e4b0b9627a307922e09f12e953e10fcd7011b00
RinandiArgoS <rinandiargo@gmail.com>
2024-09-23 21:37:01 +0700

    update2

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function About() {
     <div className="flex mt-16">
       <div className="md:w-3/4">
     <h2 className="text-left text-7xl w-full font-bold mt-20">Hi,</h2>
-    <h2 className="text-left text-7xl w-full font-bold mt-4">I'am <span class="text-orange-700">Hidayat</span></h2>
+    <h2 className="text-left text-7xl w-full font-bold mt-4">I am <span class="text-orange-700">Hidayat</span></h2>
     <h2 className="text-left text-7xl w-full font-bold mt-4">Assistant Lab</h2>
 
     <button className="mt-10   mx-1 h-9 items-center justify-center px-4  rounded-md bg-orange-500">
