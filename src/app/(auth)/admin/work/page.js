b26ee655d0c8671c92fd55683eadc97bcc1100da
hidayatnurwahid <147828701+hidayatnurwahid@users.noreply.github.com>
2024-10-07 19:51:59 +0700

    admin work_2

--- src/app/(auth)/admin/work/page.js
+++ src/app/(auth)/admin/work/page.js
@@ -1,6 +1,17 @@
+"use client"
+import { useState } from 'react';
 import Card from '../../../../components/card';
 
 export default function AdminWork() {
+  const [data, setData] = useState({
+    title:'',
+    employeType:'',
+    companyName:'',
+    location:'',
+    startDate:'',
+    endDate:'',
+  });
+
   const optEmployeType = [
     {label:'Full Time', value:'full-time'},
     {label:'Part Time', value:'part-time'},
@@ -8,56 +19,68 @@ export default function AdminWork() {
     {label:'Internship', value:'internship'}
   ]
 
-    return (<>
-        <Card title="Work Form" className="pb-5">
-          <div className="w-full my-2">
-              <label>Title</label>
-              <input type="text" className="w-full border my-input-text"/>
-          </div>
+  const inputHandler= (e) =>{
+    setData({...data, [e.target.name]: e.target.value })
+  }
 
-          <div className="w-full my-2">
-              <label>Employe Type</label>
-              <select  className="w-full border my-input-text">
-                {
-                  optEmployeType && 
-                    optEmployeType.map((item)=>
-                      <option value={item.value}>{item.label}</option>
-                    )
-                }
-              </select>
-          </div>
+  return (<>
+      <Card title="Work Form" className="pb-5">
+        <div className="w-full my-2">
+            <label>Title</label> 
+            <input 
+              type="text" 
+              name='title'
+              value={data.title}
+              onChange={inputHandler}
+              className="w-full border my-input-text"/>
+        </div>
 
-          <div className="w-full my-2">
-              <label>Company Name</label>
-              <input type="text" className="w-full border my-input-text"/>
-          </div>
+        <div className="w-full my-2">
+            <label>Employe Type</label>
+            <select  
+              name='employeType' 
+              onChange={inputHandler}
+              className="w-full border my-input-text">
+              {
+                optEmployeType && 
+                  optEmployeType.map((item, key)=>
+                    <option key={key} value={item.value}>{item.label}</option>
+                  )
+              }
+            </select>
+        </div>
 
-          <div className="w-full my-2">
-              <label>Location</label>
-              <select  className="w-full border my-input-text">
-                <option>Onsite</option>
-              </select>
-          </div>
+        <div className="w-full my-2">
+            <label>Company Name</label>
+            <input type="text" className="w-full border my-input-text"/>
+        </div>
 
-          <div className="w-full my-2">
-              <label>Start Date</label>
-              <input type="date" className="w-full border my-input-text"/>
-          </div>
+        <div className="w-full my-2">
+            <label>Location</label>
+            <select  className="w-full border my-input-text">
+              <option>Onsite</option>
+            </select>
+        </div>
 
-          <div className="w-full my-2">
-              <label>End Date</label>
-              <input type="date" className="w-full border my-input-text"/>
-          </div>
+        <div className="w-full my-2">
+            <label>Start Date</label>
+            <input type="date" className="w-full border my-input-text"/>
+        </div>
 
-          <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
-              <label>Submit Data</label>
-          </button>
-        </Card>
-        
-        <Card title="List of Work" style="mt-5">
-          asdf
-        </Card>
-      </>
-    );
+        <div className="w-full my-2">
+            <label>End Date</label>
+            <input type="date" className="w-full border my-input-text"/>
+        </div>
+
+        <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+            <label>Submit Data</label>
+        </button>
+      </Card>
+      
+      <Card title="List of Work" style="mt-5">
+        asdf
+      </Card>
+    </>
+  );
 }
   
--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,8 @@
-export default function Card({ children }) {
+export default function Card({ children,title,style }) {
     return (
-        <div className="w-full  px-4 ">
+        <div className={`w-full  px-4 my-2 ${style}`}>
             <div className=" bg-white p-10  rounded-xl">
+                <h3 className="text-2xl"></h3>
                 { children }
             </div>
         </div>
