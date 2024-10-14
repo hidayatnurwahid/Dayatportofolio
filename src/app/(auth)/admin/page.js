20b60d163c297640bf43d1ae123eb7cff944a46b
hidayatnurwahid <147828701+hidayatnurwahid@users.noreply.github.com>
2024-10-14 21:26:01 +0700

    HNurwahid_ProjectUTS

--- src/app/(auth)/admin/page.js
+++ src/app/(auth)/admin/page.js
@@ -1,8 +1,57 @@
+// pages/Dashboard.js
+"use client"
+import { useState, useEffect } from 'react';
+import Card from '../../../components/card';
+
 export default function Dashboard() {
+    const [messages, setMessages] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    async function fetchMessages() {
+        try {
+            const res = await fetch('/api/messages');
+            const data = await res.json();
+            setMessages(data.data); 
+        } catch (error) {
+            console.error("Error fetching messages:", error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchMessages();
+    }, []);
+
     return (
-      <>
-        asdf
-      </>
+        <>
+            <Card title="List Of Messages" className="pb-8">
+                <table className="min-w-full divide-y divide-gray-400">
+                    <thead>
+                        <tr>
+                            <th>#No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Subject</th>
+                            <th>Message</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className="min-w-full divide-y divide-gray-200">
+                        {!isLoading && messages.map((msg, index) => (
+                            <tr key={msg._id}>
+                                <td>{index + 1}</td>
+                                <td>{msg.name}</td>
+                                <td>{msg.email}</td>
+                                <td>{msg.subject}</td>
+                                <td>{msg.messagee}</td>
+                                {/* Api messages */}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </Card>
+            
+        </>
     );
 }
-  
--- src/app/(public)/contact/page.js
+++ src/app/(public)/contact/page.js
@@ -1,73 +1,119 @@
 "use client"
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import Card from '../../../components/card';
+
+export default function Contact() {
+    const [isLoading, setLoading] = useState(true)
+    const [data, setData] = useState({
+        name:'',
+        email:'',
+        subject:'',
+        messagee:'',
+    },
+);
+async function onLoadData() {
+    setLoading(true)
+    let res = await fetch('/api/contact')
+    let data = await res.json()
+    setData(data)
+    setLoading(false)
+}
+useEffect(() => {
+            onLoadData()
+        }, [])
 
 const ItemCard = ({label, value})=>{
     return (
-        <div className='flex gap-4 bg-white  rounded-md my-2 p-2'>
-            <div>{ label }</div>
-            <div>{ value }</div>
-        </div>
-    )
-    
-}
-
-export default function Contact(){
-    const [data, setData] = useState([])
-    const [isLoading, setLoading] = useState(true)
-
-    async function onLoadData() {
-        setLoading(true)
-        let res = await fetch('/api/contact')
-        let data = await res.json()
-        setData(data)
-        setLoading(false)
+        <div className='flex gap-4 bg-white  rounded-md m-2 p-2'>
+                <div>{ label }</div>
+                <div>{ value }</div>
+            </div>
+        )
+        
     }
+    
+    
+    
+    const inputHandler= (e) =>{
+        setData({...data, [e.target.name]: e.target.value })
+    }
+    
+    async function onSubmitData() {
+        try{
+            let res = await fetch('/api/messages', {
+                method:'POST',
+                body: JSON.stringify(data),
+            })
+            let resData = await res.json()
+            if(!resData.data){
+                throw Error(resData.message)
+            }
+            alert("Data berhasil disimpan dengan id \n"+ resData.data.insertedId)
+        }catch(err){
+            console.error("ERR", err.message)
+            alert(err.message)
+        }
+    }
+    
 
-    useEffect(() => {
-        onLoadData()
-    }, [])
-
-    return (
-        <> 
-            <h2 className="text-center text-3xl w-full">Get In Touch</h2>
+  return (<>
+    <h2 className="text-center text-3xl w-full">Get In Touch</h2>
             
-            <p className="text-center margin-0 mx-auto w-2/3	">
-                lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem 
-                ipsum lorem ipsum lorem ipsum 
-            </p>
+            <p className="mb-8 text-center margin-0 mx-auto w-2/3">
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem 
+            ipsum lorem ipsum lorem ipsum 
+        </p>
 
-            <div className="md:flex mt-16">
-                <div className="w-full md:w-3/4 px-4 ">
-                    <div className=" bg-white p-10  rounded-xl">
-                        <h3 className="text-2xl py-2">Leave a message</h3>
-                        <div className="w-ful md:flex gap-6">
-                            <div className="w-full my-2">
-                                <label>Name</label>
-                                <input type="text" className="w-full border my-input-text"/>
-                            </div>
-                            <div className="w-full my-2">
-                                <label>Email</label>
-                                <input type="text" className="w-full border my-input-text"/>
-                            </div>
-                        </div>
-                        <div className="w-full my-2">
-                            <label>Subject</label>
-                            <input type="text" className="my-input-text w-full"/>
-                        </div>
-                        <div className="w-full">
-                            <label>Message</label>
-                            <textarea className="border my-input-text w-full"></textarea>
-                        </div>
-                        <div className="w-full py-2">
-                            <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
-                                <label>Send Message</label>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className="w-full md:w-1/3 px-4">
+        <div className="flex mt-16">
+      <Card name="Work Form" className="pb-5">
+      <h3 className="text-2xl py-2">Leave a message</h3>
+         <div className="flex gap-6">
+             <div className="w-full my-2">
+                <label>Nama</label> 
+                <input 
+                  type="text" 
+                  name='name'
+                  value={data.name}
+                  onChange={inputHandler}
+                  className="w-full border my-input-text"/>
+               </div>
 
-                {
+            <div className="w-full my-2">
+                <label>Email</label>
+                <input  
+                  name='email' 
+                  onChange={inputHandler}
+                  className="w-full border my-input-text">
+                </input>
+            </div>
+           </div>
+
+        <div className="w-full my-2">
+            <label>Subject</label>
+            <input 
+              name='subject' 
+              type="text" 
+              onChange={inputHandler}
+              className="w-full border my-input-text"/>
+        </div>
+
+        <div className="w-full my-2">
+            <label>Message</label>
+            <input 
+              name='messagee'
+              onChange={inputHandler}
+              className="w-full border my-input-text">
+            </input>
+        </div>
+
+        <button 
+          onClick={onSubmitData}
+          className="mt-4 mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+            <label>Submit Data</label>
+        </button>
+      </Card>
+      <div className="md:w-1/3">
+      {
                     !isLoading && Object.keys(data.location).map(key => {
                        return <ItemCard label={key} value={data.location[key]} key={key}/>
                     })
@@ -84,11 +130,9 @@ export default function Contact(){
                        return <ItemCard label={key} value={data.social[key]} key={key}/>
                     })
                 }
-                
-                   
-                </div>
-            </div>
-
-        </>
-    );
+      </div>
+      </div>
+    </>
+  );
 }
+  
