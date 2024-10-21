e9949e3cfae4e559cd255ac3e3e53c7dbdbada40
hidayatnurwahid <147828701+hidayatnurwahid@users.noreply.github.com>
2024-10-21 21:02:57 +0700

    add photo

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,8 +1,29 @@
-export default function Dashboard() {
-    return (
-      <>
-        asdf public
-      </>
-    );
-  }
-  
+export default function About() {
+  return (
+    <>
+    <div className="flex mt-16">
+      <div className="md:w-3/4">
+    <h2 className="text-left text-7xl w-full font-bold mt-20">Hi,</h2>
+    <h2 className="text-left text-7xl w-full font-bold mt-4">I am <span class="text-orange-700">Hidayat</span></h2>
+    <h2 className="text-left text-7xl w-full font-bold mt-4">Assistant Lab</h2>
+
+    <button className="mt-10   mx-1 h-9 items-center justify-center px-4  rounded-md bg-orange-500">
+                                <label>Contact</label>
+                            </button>
+    
+                            </div>
+      <img 
+        src="/images/jajal.png" 
+        alt="Hidayat Nurwahid" 
+        layout="fill"
+        objectFit="cover"
+        width={256}
+    height={256}
+        className="rounded-full"
+      />
+      
+    </div>
+   
+    </>
+  );
+}
