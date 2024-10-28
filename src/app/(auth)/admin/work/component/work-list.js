fc4457f6a5c8665b6a5d213fe2fb6d7ad832b394
hidayatnurwahid <147828701+hidayatnurwahid@users.noreply.github.com>
2024-10-28 20:49:06 +0700

    meet_28Oct

--- src/app/(auth)/admin/work/component/work-list.js
+++ src/app/(auth)/admin/work/component/work-list.js
@@ -1,9 +1,16 @@
 "use client"
 import { useState, useEffect } from "react"
+import ConfigDialog from '../../../../../components/ConfirmDialog'
 
-export default function WorkList(){
+export default function WorkList({onEditItem}){
     const [loading, setLoading] = useState(false)
     const [data, setData] = useState([])
+    const [deletdId, setDeletdId]= useState(null)
+    const [modal, setModal] = useState(false)
+    const [modalTitle, setModalTitle] = useState("")
+    const [modalMessage, setModalMessage] = useState("")
+    const [modalBtnOk, setModalBtnOk] = useState("")
+    const [isOkOnly, setIsOkOnly]= useState(false)
 
     async function onLoadData() {
         setLoading(true)
@@ -14,16 +21,38 @@ export default function WorkList(){
     }
 
     const onDeleteItem = async (id)=>{
+        setIsOkOnly(false)
+        setModal(true);
+        setModalBtnOk("Delete");
+        setModalMessage(`Do you want to delete thes item ${id}`);
+        setModalTitle("Confirm Delete?")
+        setDeletdId(id);
+    }
+
+    const onCancel=()=>{
+        setModal(false);
+        setDeletdId(null);
+    }
+
+    const onSubmitDelete=async ()=>{
+        setModal(false);
+
         const request = {
-            deleted_id:id
+            deleted_id:deletdId
         }
         
         let res = await fetch(`/api/work`,{
             method:'DELETE',
             body: JSON.stringify(request),
-        } )
+        })
 
+        setModal(true);
+        setModalMessage(`Data Berhasil Dihapus`);
+        setModalTitle("Info")
+        setIsOkOnly(true)
+        
         onLoadData()
+
     }
 
     useEffect(() => {
@@ -32,7 +61,16 @@ export default function WorkList(){
 
     return (
         <>
-            
+            <ConfigDialog  
+                onCancel={()=>onCancel()} 
+                onOk={()=>onSubmitDelete()} 
+                onOkOny={()=>onCancel()} 
+                showDialog={modal}
+                title={modalTitle}
+                message={modalMessage}
+                okBtnMessage={modalBtnOk}
+                isOkOnly={isOkOnly} />
+
             <table className="table-auto">
                 <thead>
                     <tr>
@@ -61,7 +99,7 @@ export default function WorkList(){
                                 <td className='p-2 '>{item.endDate}</td>
                                 <td className='p-2 '>
                                     <div className="inline-flex text-[12px]">
-                                        <button className=" bg-green-300 hover:bg-green-400 text-gray-800 py-2 px-4 rounded-l">
+                                        <button onClick={()=>onEditItem(item._id)} className=" bg-green-300 hover:bg-green-400 text-gray-800 py-2 px-4 rounded-l">
                                             Edit
                                         </button>
                                         <button onClick={()=>onDeleteItem(item._id)} className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-r">
